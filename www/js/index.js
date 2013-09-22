2308e2f898799540c65e84ebcb2940eb07c5e7c0
sargered187 <thecrazybear@gmail.com>
2013-09-22 18:52:49 +1200

    Move of redirect

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+         window.location.href="http:\siti.whatsgood.net.nz";
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
